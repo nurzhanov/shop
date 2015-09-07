4a0f5512c8cbb4606f9142e9df1f31aa102bf4bc
Aleksandr Nurzhanov <aleksandr.nurzhanov@gmail.com>
2015-09-07 11:32:45 +0300

    cart is done done

--- include/itog_price.php
+++ include/itog_price.php
@@ -7,10 +7,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	if(mysqli_num_rows($result) > 0){
 		$float = 0;
 		$row = mysqli_fetch_assoc($result);
-		// while(($row=mysqli_fetch_assoc($result))!=false){
-		// 	print_r($row);
-		// 	$float = $float + ($row["cart_price"] * $row["cart_count"]);
-		// }
 		do{
 			$float = $float + ($row["cart_price"] * $row["cart_count"]);
 		}while(($row=mysqli_fetch_assoc($result))!=false);
